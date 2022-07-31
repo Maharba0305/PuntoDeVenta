dc35dbcaf67213b2b88aef6acc57e0f97eb12d61
abraham Mtz Mtz <konafreerider_05@hotmail.com>
2022-07-30 23:22:14 -0500

    Crud

---
+++ src/Datos/Interfaces/CrudSimpleInterface.java
@@ -0,0 +1,15 @@
+package Datos.Interfaces;
+
+import java.util.List;
+
+
+public interface CrudSimpleInterface<T> {
+    public List<T> listar (String texto);
+	public boolean insertar( T obj);
+	public boolean actualizar(T obj);
+	public boolean desactivar(int id);
+	public boolean activar(int id);
+	public int total();
+	public boolean exite(String texto);
+    
+}
